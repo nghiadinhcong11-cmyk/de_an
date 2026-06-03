0d148d368ed140b783fe6e1943fc5bf3c5698c52
minh080705 <nghiadinhcong11@gmail.com>
2026-06-03 11:02:13 +0700

    oke34

--- backend/Program.cs
+++ backend/Program.cs
@@ -16,12 +16,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // 2. Cấu hình CORS cực kỳ linh hoạt cho Production
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy =>
+    options.AddPolicy("CorsPolicy", policy =>
     {
-        policy.SetIsOriginAllowed(_ => true) // Cho phép tất cả origin
-              .AllowAnyMethod()
-              .AllowAnyHeader()
-              .WithExposedHeaders("Content-Disposition"); // Hữu ích cho việc download file sau này
+        policy
+            .WithOrigins(
+                "https://restaurant-pos-web.onrender.com"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
     });
 });
 
@@ -34,11 +37,12 @@ builder.Services.AddAuthentication(x =>
     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 })
-.AddJwtBearer(x =>
+.AddJwtBearer(options =>
 {
-    x.RequireHttpsMetadata = false;
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+
+    options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuerSigningKey = true,
         IssuerSigningKey = new SymmetricSecurityKey(key),
@@ -48,6 +52,23 @@ builder.Services.AddAuthentication(x =>
         ValidAudience = jwtSettings["Audience"],
         ClockSkew = TimeSpan.Zero
     };
+
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Query["access_token"];
+            var path = context.HttpContext.Request.Path;
+
+            if (!string.IsNullOrEmpty(accessToken) &&
+                path.StartsWithSegments("/notificationHub"))
+            {
+                context.Token = accessToken;
+            }
+
+            return Task.CompletedTask;
+        }
+    };
 });
 
 builder.Services.AddScoped<IAuthService, AuthService>();
@@ -56,18 +77,13 @@ builder.Services.AddOpenApi();
 builder.Services.AddSignalR(); // Thêm dòng này
 
 var app = builder.Build();
-
+app.UseCors("CorsPolicy");
 // THỨ TỰ MIDDLEWARE CỰC KỲ QUAN TRỌNG TRÊN RENDER
-app.UseCors("AllowAll"); // CORS PHẢI ĐỨNG ĐẦU TIÊN
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-// Render xử lý HTTPS ở Load Balancer, đôi khi việc redirect ở code gây lỗi CORS
-// app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
