d2993e2572774c3576a8cd43d000d9cd9903e8ff
minh080705 <nghiadinhcong11@gmail.com>
2026-06-13 19:47:15 +0700

    Add missing migrations for User, Branch and Bookings

---
+++ backend/Migrations/20260613124715_SyncLatestChanges.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantPOS.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncLatestChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Zones",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "VoucherUsages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Vouchers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UserShifts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Suppliers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Roles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Restaurants",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Refunds",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "PurchaseRecords",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "PurchaseItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ProductVariants",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Products",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "PaymentTransactions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Payments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "PaymentAccounts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Orders",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "OrderRequests",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "OrderRequestItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "OrderItems",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Ingredients",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Feedbacks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Expenses",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "DiningTables",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "CustomerSessions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Customers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "CustomerPointHistories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Categories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "BranchSettings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Branches",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Branches",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Bookings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AuditLogs",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "IsDeleted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"),
+                column: "IsDeleted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000003"),
+                column: "IsDeleted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000004"),
+                column: "IsDeleted",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Zones");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "VoucherUsages");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Vouchers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UserShifts");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Refunds");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "PurchaseRecords");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "PurchaseItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ProductVariants");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "PaymentTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "PaymentAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "OrderRequests");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "OrderRequestItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Feedbacks");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "DiningTables");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "CustomerSessions");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "CustomerPointHistories");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "BranchSettings");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Branches");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AuditLogs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Branches",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
