196f134d23887e201ae3452c197aca567ede16b4
minh080705 <nghiadinhcong11@gmail.com>
2026-06-13 19:05:14 +0700

    Remove old AppDbContext and update Dockerfile

--- backend/Infrastructure/Common/BaseEntity.cs
+++ backend/Infrastructure/Common/BaseEntity.cs
@@ -1,10 +1,10 @@
-using System;
-
-namespace RestaurantPOS.Infrastructure.Common;
-
 public abstract class BaseEntity
 {
     public Guid Id { get; set; } = Guid.NewGuid();
+
     public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
     public DateTime? UpdatedAtUtc { get; set; }
+
+    public bool IsDeleted { get; set; } = false;
 }
