33a5f271f968c8d83b87551c983f9110ce9ae655
minh080705 <nghiadinhcong11@gmail.com>
2026-06-18 13:15:45 +0700

    onks

--- restaurant_pos_web/employee/EmployeeOrders.tsx
+++ restaurant_pos_web/employee/EmployeeOrders.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../components/ui/card";
 import { Badge } from "../components/ui/badge";
 import { Button } from "../components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/tabs";
