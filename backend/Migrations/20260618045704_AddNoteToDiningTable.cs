b5f88ab86f62646f672ffbe42ebf8d85d54ecaaa
minh080705 <nghiadinhcong11@gmail.com>
2026-06-18 11:57:36 +0700

    cnwkj

---
+++ backend/Migrations/20260618045704_AddNoteToDiningTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantPOS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNoteToDiningTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "DiningTables",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "DiningTables");
+        }
+    }
+}
